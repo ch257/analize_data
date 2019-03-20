7dbe9c2ebaccdab175b851bddebaef608750ee1a
ch257 <ch257@yandex.ru>
2019-03-21 00:25:37 +0500

    ver_2_0

--- scripts/modules/common_2_0/Plotter.py
+++ scripts/modules/common_2_0/Plotter.py
@@ -14,9 +14,6 @@ class Plotter:
 		if self.errors.error_occured:
 			return None
 			
-		for section in settings:
-			print(section)
-			
 		curve_subplot = settings['curve_subplot']
 		output = settings['output']
 		subplot_height = settings['subplot_height']
@@ -26,11 +23,43 @@ class Plotter:
 		curve_color = settings['curve_color']
 		curve_alpha = settings['curve_alpha']
 		
+		# print(subplot_height)
+		
 		subplot_index = []
+		curves = []
+		subplot_heights = []
 		for col in columns:
 			if curve_subplot.get(col) != None:
-				if not curve_subplot[col] in subplot_index:
-					subplot_index.append(curve_subplot[col])
+				subp_i = int(curve_subplot[col])
+				if subplot_height.get(curve_subplot[col]) != None:
+					subp_h = int(subplot_height[curve_subplot[col]])
+				else:
+					subp_h = int(subplot_height['default'])
+				inserted = False
+				for i in range(len(subplot_index)):
+					if subplot_index[i] > subp_i:
+						m_subp_i = subplot_index[i]
+						subplot_index[i] = subp_i
+						subp_i = m_subp_i
+					
+						m_subp_h = subplot_height[i]
+						subplot_height[i] = subp_h
+						subp_h = m_subp_h
+					
+					elif subplot_index[i] == subp_i:
+						inserted = True
+						break
 				
-		subplot_index.sort()
+				if not inserted:
+					subplot_index.append(subp_i)
+					subplot_heights.append(subp_h)
+					
+				curves.append(col)
+				
+		# subplot_index.sort()
 		print(subplot_index)
+		print(subplot_heights)
+		
+		
+		
+		
---
+++ test1.bat
@@ -0,0 +1,5 @@
+@echo off
+
+python scripts\test1.py
+
+pause
